03161ea3fe9385ba1a534962e33d800e12050db2
juvekeeper41 <juvekeeper41@gmail.com>
2016-10-25 09:22:42 -0600

    Created Card Game project.

---
+++ CardGameAM/Model/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  CardGameAM
+//
+//  Created by Osborne, Jacob on 10/25/16.
+//  Copyright © 2016 Osborne, Jacob. All rights reserved.
+//
+
+import Foundation
---
+++ CardGameAM/Model/PlayingCard.swift
@@ -0,0 +1,9 @@
+//
+//  PlayingCard.swift
+//  CardGameAM
+//
+//  Created by Osborne, Jacob on 10/25/16.
+//  Copyright © 2016 Osborne, Jacob. All rights reserved.
+//
+
+import Foundation
