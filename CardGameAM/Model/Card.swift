99c6799ca774c1f88146a50081407a175bf3a5ef
juvekeeper41 <juvekeeper41@gmail.com>
2016-10-25 09:46:01 -0600

    Created the card base class with associated methods and init.

--- CardGameAM/Model/Card.swift
+++ CardGameAM/Model/Card.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2016 Osborne, Jacob. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Card
+{
+    private var backImage : UIImage
+    private var isFaceUp : Bool
+    
+    init()
+    {
+        backImage = UIImage()
+        isFaceUp = false
+    }
+    
+    func toString() -> String
+    {
+        let description = "The card is \(isFaceUp)"
+        return description
+    }
+    func getBackImage() ->UIImage
+    {
+        return backImage
+    }
+    
+    func isUp() -> Bool
+    {
+        return isFaceUp
+    }
+    
+}
